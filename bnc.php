485714b55697d2dcc93e0ee95662e1a87c2a44a8
Daniel Vygolov <rattack@gmail.com>
2019-08-04 16:11:06 +0400

    Added User Agent detection & other stuff...

--- bnc.php
+++ bnc.php
@@ -9,6 +9,7 @@ class Cloacker{
 	var $os_white;
 	var $country_white;
 	var $tokens_black;
+	var $ua_black;
 	var $ip_black;
 	var $referer;
 	var $is_bot;
@@ -25,10 +26,25 @@ class Cloacker{
 		$current_ip=$this->detect['ip'];
 		$bot_ips=file_get_contents("bots.dat");
 		$ip_black_checker=  stristr($bot_ips, $current_ip);
+		
 		if (!empty($ip_black_checker))
 		{
 			$is_bot=1;
 		}
+		
+		if(!empty($this->ua_black))
+		{
+			$ua=$this->detect['ua'];
+			$ua_black_list=explode(',',$this->ua_black);
+			foreach($ua_black_list as $ua_black_single){
+				if(!empty(stristr($ua,$ua_black_single))){
+					$result=1;
+					$this->result[]='ua';
+					break;
+				}
+			}
+		}
+		
 		if(empty($ip_black_checker)&& !empty($this->ip_black))
 		{
 			$ip_black_checker=  stristr($this->ip_black, $current_ip);
@@ -38,15 +54,22 @@ class Cloacker{
 			$result=1;
 			$this->result[]='os';
 		}
+		
 		if(!empty($this->country_white) && empty($country_white_checker) ){
 			$result=1;
 			$this->result[]='country';
 		}
-		if(!empty($ip_black_checker) ){$result=1;$this->result[]='ip';}
+		
+		if(!empty($ip_black_checker) ){
+			$result=1;
+			$this->result[]='ip';
+		}
+		
 		if(!empty($this->referer) && (int)$this->referer==1 && (int)$this->detect['referer']==0){
 			$result=1;
 			$this->result[]='referer';
 		}
+		
 		if(!empty($this->tokens_black)){
 			$this->tokens_black=explode(',',$this->tokens_black);
 			foreach($_GET AS $token){
@@ -73,6 +96,7 @@ class Cloacker{
 		$os = new Os();
 	    $a['os']=$os->getName();
 		$a['ip'] = $this->getip();
+		$a['ua']=$_SERVER['HTTP_USER_AGENT'];
 		include("geoip.inc");
 		$g = geoip_open("GeoIP.dat", GEOIP_STANDARD);
 		$a['country'] = geoip_country_code_by_addr($g, $a['ip']);
@@ -81,20 +105,34 @@ class Cloacker{
 	}
 
 	public function getip(){
-		$ipfound='Unknown';
-		if(isset($_SERVER['HTTP_X_FORWARDED_FOR']) && !empty($_SERVER['HTTP_X_FORWARDED_FOR'])){
-			$ip=$_SERVER['HTTP_X_FORWARDED_FOR'];
-			$ip=explode(", ", $ip);
-			if(count($ip)<=1){$ip=explode(",", $ip[0]);}
-			if(!empty($ip[0])){
-				$ipfound=$ip[0];
+		if (!isset($ipfound)){
+			if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+				//echo 'Client';
+				$ipfound = $_SERVER['HTTP_CLIENT_IP'];
+			} 
+		}
+		
+		if(!isset($ipfound)){
+			if(isset($_SERVER['HTTP_X_FORWARDED_FOR']) && !empty($_SERVER['HTTP_X_FORWARDED_FOR'])){
+				//echo 'Forward';
+				$ip=$_SERVER['HTTP_X_FORWARDED_FOR'];
+				$ip=explode(", ", $ip);
+				if(count($ip)<=1){$ip=explode(",", $ip[0]);}
+				if(!empty($ip[0])){
+					$ipfound=$ip[0];
+				}
 			}
 		}
+		
 		if(!isset($ipfound)){
 			if(isset($_SERVER['REMOTE_ADDR'])){
+				//echo 'Remote';
 				$ipfound=$_SERVER['REMOTE_ADDR'];
 			}
 		}
+		
+		if (!isset($ipfound))
+			$ipfound='Unknown';
 		return $ipfound;
 	}
 }
--- index.php
+++ index.php
@@ -1,52 +1,138 @@
 <?php
 //ini_set("display_errors","1"); //Для отображение отладочной информации
 require 'bnc.php';
+$gtm_id='GTM-N3BXLBQ';//ID от Google Tag Manager, если он нужен
 $full_cloak_on=0; //если 1, то всегда возвращает whitepage, используем при модерации
-$admin_ip='0.0.0.2'; //ip админа не пишется в лог посетителей
+$admin_ip='0.0.0.2'; //ip админа, не пишется в лог посетителей
 $binom_cloaker=new Cloacker();
-$binom_cloaker->os_white='Windows, Windows Phone, OS X, iOS, Android, Chrome OS, Linux, 
-    SymbOS, Nokia, BlackBerry, FreeBSD, OpenBSD, NetBSD, OpenSolaris, SunOS, 
-    OS2, BeOS'; //Список разрешённых ОС
-$binom_cloaker->country_white='RU'; //Строка двухбуквенных обозначений стран через запятую, допущенных к blackpage
+$binom_cloaker->os_white='Android,iOS,Windows'; //Список разрешённых ОС
+$binom_cloaker->country_white='ES'; //Строка двухбуквенных обозначений стран через запятую, допущенных к blackpage
 $binom_cloaker->ip_black='0.0.0.1';//Доп. список адресов через запятую, которые будут отправлены на white page
 $binom_cloaker->tokens_black=''; //Список слов через запятую, при наличии которых в адресе перехода (в ссылке, по которой перешли), юзер будет отправлен на whitepage
+$binom_cloaker->ua_black='facebook,Facebot,curl,gce-spider,yandex.com/bots'; //Список слова через запятую, при наличии которых в UserAgent, юзер будет отправлен на whitepage
 $binom_cloaker->referer='0'; //при ='1' все запросы без referer будут идти на whitepage
 $check_result=$binom_cloaker->check();
 
 //запись посетителей в файл visitors.txt
-$calledIp=$binom_cloaker->getip();
-if ($calledIp!=$admin_ip){
-  $file = "visitors.txt";
-  $time = date('Y-m-d H:i:s');  
-  $os= $binom_cloaker->detect['os'];
-  $user_agent=$_SERVER['HTTP_USER_AGENT'];
-  $reason=implode(",",$binom_cloaker->result);
-  $country=$binom_cloaker->detect['country'];
-  $message = "$calledIp, $country, $time, $check_result, $reason, $os, $user_agent \n";
-  $save_order = fopen($file, 'a+');
-  fwrite($save_order, $message);
-  fclose($save_order);
-}
-//конец записи посетителей
+write_visitors_to_log();
 
 //если включен full_cloak_on, то шлём всех на white page, полностью набрасываем плащ)
 if ($full_cloak_on==1)
 {
-	$whitePage=file_get_contents("indexw.html");
-	echo $whitePage;
+	echo load_content('/white/Mysite.html');
 	return;
 }
 
 if($check_result==0) //Обычный юзверь
 {
   //A/B тестирование лендингов
-  $landings=array("indexb.html");
+  //TODO:добавить при тестировании проброс суб-метки с номером ленда
+  $landings=array('/land/index.html');
   $r=rand(0,count($landings)-1);
-  echo file_get_contents($landings[$r]);
+  //1.если у вас html лендинг, то вам нужна эта часть кода
+  echo load_content($landings[$r]);
+  //конец части для html-лендингов
+  //2.если у вас php лендинг, то вам нужна вот эта часть кода
+  /*ob_start();
+  include $landings[$r];
+  $result = ob_get_clean();
+  echo $result;*/
+  //конец части для php лендингов
 } 
 else //Обнаружили бота или модера
 {
-	$whitePage=file_get_contents("indexw.html");
-	echo $whitePage;
+	echo load_content('/white/Mysite.html');
+}
+
+//Подгрузка контента из другой папки через CURL
+function load_content($url)
+{
+  $domain = $_SERVER['HTTP_HOST'];
+  $prefix = $_SERVER['HTTPS'] ? 'https://' : 'http://';
+  $fullpath=$prefix.$domain.$url;
+  $curl = curl_init();
+  // define options
+  $optArray = array(
+    CURLOPT_URL => $fullpath,
+    CURLOPT_RETURNTRANSFER => true,
+	CURLOPT_SSL_VERIFYPEER => false,
+   );
+  curl_setopt_array($curl, $optArray);
+  $blackbody= curl_exec($curl);
+  curl_close($curl);
+  $parts=explode('/',$url);
+  $baseurl= '/'.$parts[1].'/';
+  //переписываем все относительные src,href & action (не начинающиеся с http)
+  $blackbody = preg_replace('/\ssrc=[\'\"](?!http)([^\'\"]+)[\'\"]/', " src=\"$baseurl\\1\"", $blackbody);
+  $blackbody = preg_replace('/\shref=[\'\"](?!http|#)([^\'\"]+)[\'\"]/', " href=\"$baseurl\\1\"", $blackbody);
+  $blackbody = preg_replace('/\saction=[\'\"](?!http)([^\'\"]+)[\'\"]/', " action=\"$baseurl\\1\"", $blackbody);
+  //добавляем в страницу GTM
+  $blackbody=insert_gtm_tag($blackbody);
+  //если в querystring есть id пикселя фб, то встраиваем его скрытым полем в форму на лендинге
+  //чтобы потом передать его на страницу "Спасибо" через send.php и там отстучать Lead через GTM - норм схемка, да?))
+  $fb_pixel=$_GET["fbpixel"];
+  //если задан ID Google Tag Manager, то его ID тоже пихаем в hidden input
+  global $gtm_id;
+  $fb_input='<input type="hidden" name="fbpixel" value="'.$fb_pixel.'"/>';
+  $gtm_input='<input type="hidden" name="gtm" value="'.$gtm_id.'"/>';
+  $needle='</form>';
+  $lastPos = 0;
+  $positions = array();
+  while (($lastPos = strpos($blackbody, $needle, $lastPos))!== false) {
+	$positions[] = $lastPos;
+	$lastPos = $lastPos + strlen($needle);
+  }
+  $positions=array_reverse($positions);
+  
+  foreach ($positions as $pos) {
+	if (!empty($fb_pixel)){
+      $blackbody=substr_replace($blackbody,$fb_input,$pos,0);
+	}
+	if (!empty($gtm_id)){
+	  $blackbody=substr_replace($blackbody,$gtm_input,$pos,0);
+	}
+  }
+ 
+  return $blackbody;
+}
+
+function insert_gtm_tag($html){
+	global $gtm_id;
+	if (!empty($gtm_id)){
+		$needle='</head>';
+		$pos=strpos($html,$needle,0);
+		$gtm_text="<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','".$gtm_id."');</script>";
+		$html=substr_replace($html,$gtm_text,$pos,0);
+	}
+	return $html;
+}
+
+//Запись всех посетителей в csv файл
+function write_visitors_to_log()
+{
+	global $binom_cloaker, $admin_ip, $check_result;
+	$calledIp=$binom_cloaker->detect['ip'];
+	
+	if ($calledIp!=$admin_ip){
+	  $file = "visitors.csv";
+	  $time = date('Y-m-d H:i:s');  
+	  $os= $binom_cloaker->detect['os'];
+	  $user_agent=$binom_cloaker->detect['ua'];
+	  
+	  if (isset($binom_cloaker->result))
+		$reason=implode(";",$binom_cloaker->result);
+	  else
+		$reason="";
+	  $country=$binom_cloaker->detect['country'];
+	  $message = "$calledIp, $country, $time, $check_result, $reason, $os, $user_agent \n";
+	  $save_order = fopen($file, 'a+');
+	  fwrite($save_order, $message);
+	  fflush($save_order);
+	  fclose($save_order);
+	}
 }
 ?>
