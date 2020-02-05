c8c5866099a48b18da780410efa9913f0161d2d7
Klepov Yura <onword333@gmail.com>
2020-02-05 19:06:59 +0300

    Добавлена разметка для черной и белой страницы

--- land1/index.php
+++ land1/index.php
@@ -1,5 +1,12 @@
-<?php
-
-echo 'BAD_PAGE';
-
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Black page</title>
+</head>
+<body>
+  <h1>Black page</h1>  
+</body>
+</html>
--- white/index.php
+++ white/index.php
@@ -1,5 +1,12 @@
-<?php
-
-echo 'WHITE PAGE';
-
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>White page</title>
+</head>
+<body>
+  <h1>White page</h1>  
+</body>
+</html>
