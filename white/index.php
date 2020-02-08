d2aab877ea8ade9bb2d6f348452bf88175c209d8
Klepov Yura <onword333@gmail.com>
2020-02-08 21:06:47 +0300

    Подключены файл отправки (send.php) и страница "Спасибо"

--- white/index.php
+++ white/index.php
@@ -1,12 +1,23 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ru">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>White page</title>
+  <link href="css/style.css" type="text/css" rel="stylesheet">
+  <link rel="icon" href="../favicon.ico" type="image/x-icon">
+  <link rel="shortcut icon" href="../favicon.ico" type="image/x-icon">
+
 </head>
 <body>
-  <h1>White page</h1>  
+  <h1>White page</h1>
+  <h2>Это демонстрационный пример</h2>
+  <form method="POST" action="../send.php">
+    <input type="text" name="email">
+    <input type="text" name="name">
+    <button type="submit">Заказать</button>
+  </form>
+  <script src="js/jquery-3.4.1.min.js"></script>
 </body>
 </html>
