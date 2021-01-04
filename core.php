51f27374cfe7002344ad805018e177fa3c3addb6
Daniel Vygolov <rattack@gmail.com>
2021-01-04 22:13:01 +0400

    мелочи для blackbox

--- core.php
+++ core.php
@@ -50,23 +50,17 @@ class Cloaker{
 	}
 
 	private function blackbox($ip){
-        // Get file.
         $url = 'https://blackbox.ipinfo.app/lookup/';
         $res = file_get_contents($url . $ip);
 
-        // Check to make sure we have a valid response.
         if(!is_string($res) || !strpos($http_response_header[0], "200")){
-            // We didn't get what we were looking for.
-            //http_response_code(503);
-            exit("We didn't get what we were looking for.");
+			return false;
         }
 
-        // Is found to be proxy in result, return !
         if($res !== null && $res === 'Y'){
             return true;
         }
 
-        // return result
         return false;
     }
 
