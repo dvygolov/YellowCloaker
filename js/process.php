e367901b6472ba282d09da80844deb9817b53f19
Daniel Vygolov <rattack@gmail.com>
2021-01-07 17:37:30 +0400

    Правка замены контента вайта на конструкторах

--- js/process.php
+++ js/process.php
@@ -2,8 +2,10 @@
     include 'obfuscator.php';
     include '../settings.php';
     header('Content-Type: text/javascript');
-    $jsCode= str_replace('{DOMAIN}', $_SERVER['SERVER_NAME'], file_get_contents(__DIR__.'/process.js'));
-    $jsCode= str_replace('{REASON}', $_GET['reason'], $jsCode); //пробрасываем js-причину того, что разрешаем переход на блэк
+	$jsCode= str_replace('{DOMAIN}', $_SERVER['SERVER_NAME'], file_get_contents(__DIR__.'/process.js'));
+	$reason='nojschecks';
+	if (!empty($_GET['reason'])) $reason=$_GET['reason'];
+    $jsCode= str_replace('{REASON}', $reason, $jsCode); //пробрасываем js-причину того, что разрешаем переход на блэк
     if ($js_obfuscate) {
         $hunter = new HunterObfuscator($jsCode);
         echo $hunter->Obfuscate();
