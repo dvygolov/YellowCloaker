3be04b7b63dcf61b638ef52c9b0daf21c367c312
Daniel Vygolov <rattack@gmail.com>
2021-01-18 14:38:52 +0400

    Port Bug

--- js/process.php
+++ js/process.php
@@ -1,8 +1,10 @@
 <?php
     include_once 'obfuscator.php';
     include_once '../settings.php';
+	include_once '../requestfunc.php';
     header('Content-Type: text/javascript');
-	$jsCode= str_replace('{DOMAIN}', $_SERVER['SERVER_NAME'].":".$_SERVER['SERVER_PORT'], file_get_contents(__DIR__.'/process.js'));
+	$port = get_port();
+	$jsCode= str_replace('{DOMAIN}', $_SERVER['SERVER_NAME'].":".$port, file_get_contents(__DIR__.'/process.js'));
 	$reason='nojschecks';
 	if (!empty($_GET['reason'])) $reason=$_GET['reason'];
     $jsCode= str_replace('{REASON}', $reason, $jsCode); //пробрасываем js-причину того, что разрешаем переход на блэк
