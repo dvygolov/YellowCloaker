6ad0b29707c446da3164b4ff85adc4b3edc8c8f9
Daniel Vygolov <rattack@gmail.com>
2021-01-18 15:07:24 +0400

    Log js bug

--- js/logjsbot.php
+++ js/logjsbot.php
@@ -4,9 +4,10 @@ ini_set('display_errors', '1');
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 //Конец включения отладочной информации
+include '../settings.php';
 require '../core.php';
 include '../logging.php';
 
-$cloacker = new Cloacker();
-write_white_to_log($cloacker->detect, ['js_tests'], 1, '', '');
+$cloaker = new Cloaker($os_white,$country_white,$ip_black_filename,$ip_black_cidr,$tokens_black,$url_should_contain,$ua_black,$isp_black,$block_without_referer,$block_vpnandtor);
+write_white_to_log($cloaker->detect, ['js_tests'], 1, '', '');
 ?>
