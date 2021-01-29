30989e808c04e8bf20d1eb919e095e261fde45f4
Daniel Vygolov <rattack@gmail.com>
2021-01-29 17:30:40 +0400

    начал пилить автоапселлы

---
+++ thankyou/carousel/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $('.carousel').slick({
+  slidesToShow: 3,
+  dots:true,
+  centerMode: false,
+  variableWidth: true,
+  autoplay: true,
+  autoplaySpeed: 2000
+  });
+});
---
+++ thankyou/upsell.js
@@ -0,0 +1,3 @@
+<script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+<script src='https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.5.8/slick.min.js'></script>
+<script src="carousel/script.js"></script>
