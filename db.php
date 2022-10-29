04ee7f133ffaae0454e29bf3a1f0d0b2091e0782
Yellow Web <rattack@gmail.com>
2022-10-29 19:01:34 +0400

    вывожу ошибки при постбэке

--- db.php
+++ db.php
@@ -1,177 +1,184 @@
 <?php
-require_once __DIR__."/db/Classes/IoHelper.php";
-require_once __DIR__."/db/SleekDB.php";
-require_once __DIR__."/db/Store.php";
-require_once __DIR__."/db/QueryBuilder.php";
-require_once __DIR__."/db/Query.php";
-require_once __DIR__."/db/Cache.php";
-require_once __DIR__."/cookies.php";
+require_once __DIR__ . "/db/Classes/IoHelper.php";
+require_once __DIR__ . "/db/SleekDB.php";
+require_once __DIR__ . "/db/Store.php";
+require_once __DIR__ . "/db/QueryBuilder.php";
+require_once __DIR__ . "/db/Query.php";
+require_once __DIR__ . "/db/Cache.php";
+require_once __DIR__ . "/cookies.php";
 
 use SleekDB\Store;
 
-function add_white_click($data,$reason) {
+function add_white_click($data, $reason)
+{
     $dataDir = __DIR__ . "/logs";
     $wclicksStore = new Store("whiteclicks", $dataDir);
 
-	$calledIp = $data['ip'];
-	$country = $data['country'];
-	$dt = new DateTime();
-	$time = $dt->getTimestamp();
-	$os = $data['os'];
-	$isp = str_replace(',',' ',$data['isp']);
-	$user_agent = str_replace(',',' ',$data['ua']);
+    $calledIp = $data['ip'];
+    $country = $data['country'];
+    $dt = new DateTime();
+    $time = $dt->getTimestamp();
+    $os = $data['os'];
+    $isp = str_replace(',', ' ', $data['isp']);
+    $user_agent = str_replace(',', ' ', $data['ua']);
 
-	parse_str($_SERVER['QUERY_STRING'],$queryarr);
+    parse_str($_SERVER['QUERY_STRING'], $queryarr);
 
-	$click =[
-		"time"=>$time,
-		"ip"=>$calledIp,
-		"country"=>$country,
-		"os"=>$os,
-		"isp"=>$isp,
-		"ua"=>$user_agent,
-		"reason"=>$reason,
-		"subs"=>$queryarr
+    $click = [
+        "time" => $time,
+        "ip" => $calledIp,
+        "country" => $country,
+        "os" => $os,
+        "isp" => $isp,
+        "ua" => $user_agent,
+        "reason" => $reason,
+        "subs" => $queryarr
     ];
-	$wclicksStore->insert($click);
+    $wclicksStore->insert($click);
 }
 
-function add_black_click($subid,$data,$preland,$land) {
+function add_black_click($subid, $data, $preland, $land)
+{
     $dataDir = __DIR__ . "/logs";
     $bclicksStore = new Store("blackclicks", $dataDir);
 
-	$calledIp = $data['ip'];
-	$country = $data['country'];
-	$dt = new DateTime();
-	$time = $dt->getTimestamp();
-	$os = $data['os'];
-	$isp = str_replace(',',' ',$data['isp']);
-	$user_agent = str_replace(',',' ',$data['ua']);
-	$prelanding=empty($preland)?'unknown':$preland;
-	$landing=empty($land)?'unknown':$land;
+    $calledIp = $data['ip'];
+    $country = $data['country'];
+    $dt = new DateTime();
+    $time = $dt->getTimestamp();
+    $os = $data['os'];
+    $isp = str_replace(',', ' ', $data['isp']);
+    $user_agent = str_replace(',', ' ', $data['ua']);
+    $prelanding = empty($preland) ? 'unknown' : $preland;
+    $landing = empty($land) ? 'unknown' : $land;
 
-	parse_str($_SERVER['QUERY_STRING'],$queryarr);
+    parse_str($_SERVER['QUERY_STRING'], $queryarr);
 
-	$click =[
-		"subid"=>$subid,
-		"time"=>$time,
-		"ip"=>$calledIp,
-		"country"=>$country,
-		"os"=>$os,
-		"isp"=>$isp,
-		"ua"=>$user_agent,
-		"subs"=>$queryarr,
-		"preland"=>$prelanding,
-		"land"=>$landing
+    $click = [
+        "subid" => $subid,
+        "time" => $time,
+        "ip" => $calledIp,
+        "country" => $country,
+        "os" => $os,
+        "isp" => $isp,
+        "ua" => $user_agent,
+        "subs" => $queryarr,
+        "preland" => $prelanding,
+        "land" => $landing
     ];
-	$bclicksStore->insert($click);
+    $bclicksStore->insert($click);
 }
 
-function add_lead($subid,$name,$phone,$status='Lead') {
+function add_lead($subid, $name, $phone, $status = 'Lead')
+{
     $dataDir = __DIR__ . "/logs";
     $leadsStore = new Store("leads", $dataDir);
 
-	$fbp=get_cookie('_fbp');
-	$fbclid=get_cookie('fbclid');
-	if ($fbclid==='') $fbclid=get_cookie('_fbc');
+    $fbp = get_cookie('_fbp');
+    $fbclid = get_cookie('fbclid');
+    if ($fbclid === '') $fbclid = get_cookie('_fbc');
 
-	if ($status=='') $status='Lead';
+    if ($status == '') $status = 'Lead';
 
-	$dt = new DateTime();
-	$time = $dt->getTimestamp();
+    $dt = new DateTime();
+    $time = $dt->getTimestamp();
 
-	$land = get_cookie('landing');
-	if (empty($land)) $land='unknown';
-	$preland = get_cookie('prelanding');
-	if (empty($preland)) $preland='unknown';
+    $land = get_cookie('landing');
+    if (empty($land)) $land = 'unknown';
+    $preland = get_cookie('prelanding');
+    if (empty($preland)) $preland = 'unknown';
 
-	$lead=[
-		"subid"=>$subid,
-		"time"=>$time,
-		"name"=>$name,
-		"phone"=>$phone,
-		"status"=>$status,
-		"fbp"=>$fbp,
-		"fbclid"=>$fbclid,
-		"preland"=>$preland,
-		"land" => $land
+    $lead = [
+        "subid" => $subid,
+        "time" => $time,
+        "name" => $name,
+        "phone" => $phone,
+        "status" => $status,
+        "fbp" => $fbp,
+        "fbclid" => $fbclid,
+        "preland" => $preland,
+        "land" => $land
     ];
-	return $leadsStore->insert($lead);
+    return $leadsStore->insert($lead);
 }
 
-function update_lead($subid,$status,$payout){
+function update_lead($subid, $status, $payout)
+{
     $dataDir = __DIR__ . "/logs";
     $leadsStore = new Store("leads", $dataDir);
-    $lead=$leadsStore->findOneBy([["subid","=",$subid]]);
-	if ($lead===null){
+    $lead = $leadsStore->findOneBy([["subid", "=", $subid]]);
+    if ($lead === null) {
         $bclicksStore = new Store("blackclicks", $dataDir);
-        $click=$bclicksStore->findOneBy([["subid","=",$subid]]);
-		if ($click===null) return false;
-		$lead=add_lead($subid,'','');
+        $click = $bclicksStore->findOneBy([["subid", "=", $subid]]);
+        if ($click === null) return false;
+        $lead = add_lead($subid, '', '');
     }
 
-	$lead["status"]=$status;
-	$lead["payout"]=$payout;
+    $lead["status"] = $status;
+    $lead["payout"] = $payout;
     $leadsStore->update($lead);
-	return true;
+    return true;
 }
 
-function email_exists_for_subid($subid){
+function email_exists_for_subid($subid)
+{
     $dataDir = __DIR__ . "/logs";
     $leadsStore = new Store("leads", $dataDir);
-	$lead=$leadsStore->findOneBy([["subid","=",$subid]]);
-	if ($lead===null) return false;
-    if (array_key_exists("email",$lead)) return true;
-	return false;
+    $lead = $leadsStore->findOneBy([["subid", "=", $subid]]);
+    if ($lead === null) return false;
+    if (array_key_exists("email", $lead)) return true;
+    return false;
 }
 
-function add_email($subid,$email){
+function add_email($subid, $email)
+{
     $dataDir = __DIR__ . "/logs";
     $leadsStore = new Store("leads", $dataDir);
-	$lead=$leadsStore->findOneBy([["subid","=",$subid]]);
-	if ($lead===null) return;
-	$lead["email"]=$email;
-	$leadsStore->update($lead);
+    $lead = $leadsStore->findOneBy([["subid", "=", $subid]]);
+    if ($lead === null) return;
+    $lead["email"] = $email;
+    $leadsStore->update($lead);
 }
 
-function add_lpctr($subid,$preland){
+function add_lpctr($subid, $preland)
+{
     $dataDir = __DIR__ . "/logs";
     $lpctrStore = new Store("lpctr", $dataDir);
-	$dt = new DateTime();
-	$time = $dt->getTimestamp();
+    $dt = new DateTime();
+    $time = $dt->getTimestamp();
 
-	$lpctr = [
-		"time"=>$time,
-		"subid"=>$subid,
-		"preland"=>$preland
+    $lpctr = [
+        "time" => $time,
+        "subid" => $subid,
+        "preland" => $preland
     ];
-	$lpctrStore->insert($lpctr);
+    $lpctrStore->insert($lpctr);
 }
 
 //проверяем, есть ли в файле лидов subid текущего пользователя
 //если есть, и также есть такой же номер - значит ЭТО ДУБЛЬ!
 //И нам не нужно слать его в ПП и не нужно показывать пиксель ФБ!!
-function lead_is_duplicate($subid,$phone){
+function lead_is_duplicate($subid, $phone)
+{
     $dataDir = __DIR__ . "/logs";
     $leadsStore = new Store("leads", $dataDir);
-	if($subid!=''){
-        $lead=$leadsStore->findOneBy([["subid","=",$subid]]);
-        if ($lead===null) return false;
+    if ($subid != '') {
+        $lead = $leadsStore->findOneBy([["subid", "=", $subid]]);
+        if ($lead === null) return false;
         header("YWBDuplicate: We have this sub!");
-        $phoneexists = ($lead["phone"]===$phone);
-        if ($phoneexists){
+        $phoneexists = ($lead["phone"] === $phone);
+        if ($phoneexists) {
             header("YWBDuplicate: We have this phone!");
             return true;
-        }
-        else{
+        } else {
             return false;
         }
-	}
-	else {
-		//если куки c subid у нас почему-то нет, то проверяем по номеру телефона
-        $lead=$leadsStore->findOneBy([["phone","=",$phone]]);
-		if ($lead===null) return false;
-		return true;
-	}
+    } else {
+        //если куки c subid у нас почему-то нет, то проверяем по номеру телефона
+        $lead = $leadsStore->findOneBy([["phone", "=", $phone]]);
+        if ($lead === null) return false;
+        return true;
+    }
 }
+
 ?>
--- postback.php
+++ postback.php
@@ -1,6 +1,6 @@
 <?php
 //Включение отладочной информации
-ini_set('display_errors','1');
+ini_set('display_errors', '1');
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 //Конец включения отладочной информации
@@ -10,80 +10,93 @@ require_once 'db.php';
 require_once 'url.php';
 require_once 'requestfunc.php';
 
-function add_s2s_log($url,$status,$method,$s2s_res){
+$curLink = 'http' . (isset($_SERVER['HTTPS']) ? 's' : '') . '://' . "{$_SERVER['HTTP_HOST']}{$_SERVER['REQUEST_URI']}";
+$subid = $_REQUEST['subid'] ?? '';
+if ($subid ===''){
+    echo "No subid found! Url: $curLink";
+    return;
+}
+$status = $_REQUEST['status'] ?? '';
+if ($status ===''){
+    echo "No status found! Url: $curLink";
+    return;
+}
+$payout = $_REQUEST['payout'] ?? '';
+if ($payout ===''){
+    echo "No payout found! Url: $curLink";
+    return;
+}
+$inner_status = '';
+switch ($status) {
+    case $lead_status_name:
+        $inner_status = 'Lead';
+        break;
+    case $purchase_status_name:
+        $inner_status = 'Purchase';
+        break;
+    case $reject_status_name:
+        $inner_status = 'Reject';
+        break;
+    case $trash_status_name:
+        $inner_status = 'Trash';
+        break;
+}
+add_postback_log($subid, $inner_status, $payout);
+$res = update_lead($subid, $inner_status, $payout);
+foreach ($s2s_postbacks as $s2s) {
+    if (!in_array($inner_status, $s2s['events'])) continue;
+    if (empty($s2s['url'])) continue;
+    $final_url = replace_all_macros($s2s['url']);
+    $final_url = str_replace('{status}', $inner_status, $final_url);
+    switch ($s2s['method']) {
+        case 'GET':
+            $s2s_res = get($final_url);
+            break;
+        case 'POST':
+            $urlParts = explode('?', $final_url);
+            if (count($urlParts) == 1)
+                $params = array();
+            else
+                parse_str($urlParts[1], $params);
+            $s2s_res = post($urlParts[0], $params);
+            break;
+    }
+    add_s2s_log($final_url, $inner_status, $s2s['method'], $s2s_res);
+}
+
+if ($res)
+    echo "Postback for subid $subid with status $status accepted";
+else
+    echo "Postback for subid $subid with status $status NOT accepted!";
+
+function add_s2s_log($url, $status, $method, $s2s_res)
+{
     //создаёт папку для лога постбэков, если её нет
-    if (!file_exists(__DIR__."/pblogs")) mkdir(__DIR__."/pblogs");
-    $file = __DIR__."/pblogs/".date("d.m.y").".pb.log";
+    if (!file_exists(__DIR__ . "/pblogs")) mkdir(__DIR__ . "/pblogs");
+    $file = __DIR__ . "/pblogs/" . date("d.m.y") . ".pb.log";
     $save_order = fopen($file, 'a+');
-    fwrite($save_order, date("Y-m-d H:i:s")." $method $url $status {$s2s_res['info']['http_code']}\n");
+    fwrite($save_order, date("Y-m-d H:i:s") . " $method $url $status {$s2s_res['info']['http_code']}\n");
     fflush($save_order);
     fclose($save_order);
 }
 
-function add_postback_log($subid,$status,$payout){
+function add_postback_log($subid, $status, $payout)
+{
     //создаёт папку для лога постбэков, если её нет
-    if (!file_exists(__DIR__."/pblogs")) mkdir(__DIR__."/pblogs");
-    $file = __DIR__."/pblogs/".date("d.m.y").".pb.log";
+    if (!file_exists(__DIR__ . "/pblogs")) mkdir(__DIR__ . "/pblogs");
+    $file = __DIR__ . "/pblogs/" . date("d.m.y") . ".pb.log";
     $save_order = fopen($file, 'a+');
-    if ($subid==='' || $status==='') {
-        $curLink = 'http' . (isset($_SERVER['HTTPS']) ? 's' : '') . '://' . "{$_SERVER['HTTP_HOST']}/{$_SERVER['REQUEST_URI']}";
-        $err_msg=date("Y-m-d H:i:s")." Error! No subid or status! {$curLink}\n";
+    if ($subid === '' || $status === '') {
+        $err_msg = date("Y-m-d H:i:s") . " Error! No subid or status! {$curLink}\n";
         fwrite($save_order, $err_msg);
         fflush($save_order);
         fclose($save_order);
         die($err_msg);
-    }
-    else{
-        fwrite($save_order, date("Y-m-d H:i:s")." $subid, $status, $payout\n");
+    } else {
+        fwrite($save_order, date("Y-m-d H:i:s") . " $subid, $status, $payout\n");
         fflush($save_order);
         fclose($save_order);
     }
 }
 
-$subid= isset($_REQUEST['subid'])?$_REQUEST['subid']:'';
-$status= isset($_REQUEST['status'])?$_REQUEST['status']:'';
-$payout= isset($_REQUEST['payout'])?$_REQUEST['payout']:'';
-
-$inner_status='';
-switch($status){
-    case $lead_status_name:
-        $inner_status='Lead';
-        break;
-    case $purchase_status_name:
-        $inner_status='Purchase';
-        break;
-    case $reject_status_name:
-        $inner_status='Reject';
-        break;
-    case $trash_status_name:
-        $inner_status='Trash';
-        break;
-}
-add_postback_log($subid,$inner_status,$payout);
-$res=update_lead($subid,$inner_status,$payout);
-foreach($s2s_postbacks as $s2s){
-    if (!in_array($inner_status,$s2s['events'])) continue;
-    if (empty($s2s['url'])) continue;
-    $final_url=replace_all_macros($s2s['url']);
-    $final_url=str_replace('{status}',$inner_status,$final_url);
-    switch($s2s['method']){
-        case 'GET':
-            $s2s_res=get($final_url);
-            break;
-        case 'POST':
-            $urlParts=explode('?',$final_url);
-            if (count($urlParts)==1)
-                $params=array();
-            else
-                parse_str($urlParts[1],$params);
-            $s2s_res=post($urlParts[0],$params);
-            break;
-    }
-    add_s2s_log($final_url,$inner_status,$s2s['method'],$s2s_res);
-}
-
-if($res)
-    echo "Postback for subid $subid with status $status accepted";
-else
-    echo "Postback for subid $subid with status $status NOT accepted!";
 ?>
