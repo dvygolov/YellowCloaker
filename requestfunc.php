5404ca76cb5d0bad76c97564581d174b55fd8413
tiagobonacina <tiagobonacina@gmail.com>
2022-12-25 07:33:31 -0300

    Update requestfunc.php
    Fix BAD REQUEST error in some web servers due to missing ":" after X-FORWARDED-FOR header

--- requestfunc.php
+++ requestfunc.php
@@ -34,7 +34,7 @@ function get_request_headers($ispost=false){
 	$ip=getip();
     $headers=array(
 				'X-YWBCLO-UIP: '.$ip,
-				'X-FORWARDED-FOR '.$ip,
+				'X-FORWARDED-FOR: '.$ip,
 				//'CF-CONNECTING-IP: '.$ip,
 				'FORWARDED-FOR: '.$ip,
 				'X-COMING-FROM: '.$ip,
