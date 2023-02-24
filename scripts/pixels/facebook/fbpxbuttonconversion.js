1d0430a553025375b3a492dc2decf5e6bf784987
Daniel Vygolov <rattack@gmail.com>
2023-02-24 19:21:47 +0400

    JS-refactoring bugs.

--- scripts/pixels/facebook/fbpxbuttonconversion.js
+++ scripts/pixels/facebook/fbpxbuttonconversion.js
@@ -2,8 +2,7 @@ window.addEventListener('DOMContentLoaded', () => {
     const handleEvent = (event) => {
         event.preventDefault();
         console.log('Started Facebook pixel firing...');
-        const element = event.currentTarget;
-        let node = element;
+        let node = event.currentTarget;
         while (node.nodeName !== 'FORM' && node.parentNode) {
             node = node.parentNode;
         }
@@ -11,13 +10,13 @@ window.addEventListener('DOMContentLoaded', () => {
         const name = node.querySelector('input[name="name"]');
         const phone = node.querySelector('input[name="phone"]');
 
-        if (name.value != '' && phone.value != '') {
+        if (name.value !== '' && phone.value !== '') {
             fbq('track', '{EVENT}');
         }
     };
 
-    const forms = document.querySelectorAll('form button, form input[type="submit"]');
-    forms.forEach((form) => {
-        form.addEventListener('click', handleEvent);
+    const formButtons = document.querySelectorAll('form button, form input[type="submit"]');
+    formButtons.forEach((button) => {
+        button.addEventListener('click', handleEvent);
     });
 });
