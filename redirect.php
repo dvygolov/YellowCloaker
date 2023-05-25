05441b6d5b663b5e99f8a7b886d120d32502fa99
Daniel Vygolov <rattack@gmail.com>
2023-05-25 17:39:13 +0400

    Added         header('X-Robots-Tag: noindex, nofollow');

--- redirect.php
+++ redirect.php
@@ -1,19 +1,22 @@
 <?php
 require_once 'url.php';
 
-function redirect($url,$redirect_type=302,$add_querystring=true){
-    if ($add_querystring===true){
-        $url=add_querystring($url);
+function redirect($url, $redirect_type = 302, $add_querystring = true)
+{
+    if ($add_querystring === true) {
+        $url = add_querystring($url);
     }
-    if ($redirect_type===302) {
-        header('Location: '.$url);
+    if ($redirect_type === 302) {
+        header('X-Robots-Tag: noindex, nofollow');
+        header('Location: ' . $url);
     } else {
-        header('Location: '.$url, true, $redirect_type);
+        header('X-Robots-Tag: noindex, nofollow');
+        header('Location: ' . $url, true, $redirect_type);
     }
 }
 
-function jsredirect($url){
+function jsredirect($url)
+{
     echo "<script type='text/javascript'> window.location='$url';</script>";
     return;
 }
-?>
