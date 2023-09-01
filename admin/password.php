4b1600ad3bea4829288a17e0e70462b8f2ad9107
Daniel Vygolov <rattack@gmail.com>
2023-09-01 14:09:13 +0400

    Paths fix + Long PHP Version fix

--- admin/password.php
+++ admin/password.php
@@ -1,5 +1,5 @@
 <?php
-require_once '../settings.php';
+require_once __DIR__.'/../settings.php';
 
 function check_password(){
     global $log_password;
--- admin/version.php
+++ admin/version.php
@@ -1,8 +1,8 @@
 <h2>
     <a href="https://yellowweb.top/donate" target="_blank">
-        Version: 19.10.2022
-        <br/>
-        PHP Version: <?=phpversion()?>
-        <br/> Please, donate!
+        Version: 01.09.2023
+        <br />
+        PHP: <?= phpversion() ?>
+        <br /> Please, donate!
     </a>
 </h2>
