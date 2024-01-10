9fd384abbdf099884b41a4d5c9f180afc826f909
Yellow Web <rattack@gmail.com>
2024-01-10 23:32:27 +0400

    table columns beta

--- admin/index.php
+++ admin/index.php
@@ -2,25 +2,22 @@
 global $startdate, $enddate, $config;
 require_once __DIR__ . '/../db.php';
 require_once __DIR__ . '/initialization.php';
+require_once __DIR__ . '/tablecolumns.php';
 
 $filter = $_GET['filter'] ?? '';
 $db = new Db();
 switch ($filter) {
     case 'leads':
-        $header = ["Subid", "Time", "Name", "Phone", "Status", "Preland", "Land"];
         $dataset = $db->get_leads($startdate->getTimestamp(), $enddate->getTimestamp(), $config);
         break;
     case 'blocked':
-        $header = ["IP", "Country", "ISP", "Time", "Reason", "OS", "UA", "Subs"];
         $dataset = $db->get_white_clicks($startdate->getTimestamp(), $enddate->getTimestamp(), $config);
         break;
     case 'single':
-        $header = ["Subid", "IP", "Country", "ISP", "Time", "OS", "UA", "Subs", "Preland", "Land"];
         $click = $_GET['subid'] ?? '';
         $dataset = $db->get_single_click($click, $config);
         break;
     default:
-        $header = ["Subid", "IP", "Country", "ISP", "Time", "OS", "UA", "Subs", "Preland", "Land"];
         $dataset = $db->get_black_clicks($startdate->getTimestamp(), $enddate->getTimestamp(), $config);
         break;
 }
@@ -37,39 +34,16 @@ switch ($filter) {
     </div>
     <script>
         let tableData = <?= json_encode($dataset) ?>;
+        let tableColumns = <?=get_columns($filter) ?>;
         let table = new Tabulator('#clicks', {
             layout: "fitColumns",
+            columns: tableColumns,
             pagination: "local",
             paginationSize: 25,
             paginationSizeSelector: [25, 50, 100, 200, 500],
             paginationCounter: "rows",
             height: "100%",
             data: tableData,
-            autoColumns: true,
-            autoColumnsDefinitions: {
-                _id: {
-                    title: "#",
-                    formatter: "rownum"
-                },
-                config: {
-                    visible: false
-                },
-                fbp: {
-                    visible: false
-                },
-                fbclid: {
-                    visible: false
-                },
-                reason: {
-                    visible: window.location.href.includes("filter=blocked")
-                },
-                subid: {
-                    formatter: "link",
-                    formatterParams: {
-                        urlPrefix: "index.php?filter=single&subid="
-                    }
-                }
-            },
         });
     </script>
 </body>
---
+++ admin/tablecolumns.php
@@ -0,0 +1,99 @@
+<?php
+function get_columns(string $filter): string
+{
+    $columns = [];
+    switch($filter){
+    case 'blocked':
+        $columns = [
+            ["title"=>"IP","field"=>"ip"],
+            ["title"=>"Country","field"=>"country", "headerFilter"=>"input"], 
+            ["title"=>"ISP","field"=>"isp", "headerFilter"=>"input"], 
+            ["title"=>"Time","field"=>"time"], 
+            ["title"=>"Reason","field"=>"reason", "formatter"=>"plaintext"], 
+            ["title"=>"OS","field"=>"os", "headerFilter"=>"input"], 
+            ["title"=>"UA","field"=>"ua", "formatter"=>"textarea"], 
+            ["title"=>"Subs","field"=>"subs"]
+        ];
+        break;
+    case 'single':
+        $columns = [
+            ["title"=>"Subid","field"=>"subid"], 
+            ["title"=>"IP","field"=>"iP"], 
+            ["title"=>"Country","field"=>"country"], 
+            ["title"=>"ISP","field"=>"isp"], 
+            ["title"=>"Time","field"=>"time"], 
+            ["title"=>"OS","field"=>"os"], 
+            ["title"=>"UA","field"=>"ua"], 
+            ["title"=>"Subs","field"=>"subs"], 
+            ["title"=>"Preland","field"=>"preland"], 
+            ["title"=>"Land","field"=>"land"]
+        ];
+        break;
+    case 'leads':
+        $columns = [
+            ["title"=>"Subid","field"=>"subid", 
+             "formatter"=>"link", 
+             "formatterParams"=>["urlPrefix"=>"index.php?filter=single&subid=" ] ], 
+            ["title"=>"Time","field"=>"time"], 
+            ["title"=>"Name","field"=>"name"], 
+            ["title"=>"Phone","field"=>"phone"], 
+            ["title"=>"Status","field"=>"status"], 
+            ["title"=>"Preland","field"=>"preland"], 
+            ["title"=>"Land","field"=>"land"]
+        ];
+        break;
+    default:
+        $columns = [
+            ["title"=>"Subid","field"=>"subid", 
+             "formatter"=>"link", 
+             "formatterParams"=>["urlPrefix"=>"index.php?filter=single&subid=" ] ], 
+            ["title"=>"IP","field"=>"ip", "headerFilter"=>"input"], 
+            ["title"=>"Country","field"=>"country", "headerFilter"=>"input"], 
+            ["title"=>"ISP","field"=>"isp", "headerFilter"=>"input"], 
+            ["title"=>"Time","field"=>"time"], 
+            ["title"=>"OS","field"=>"os", "headerFilter"=>"input"], 
+            ["title"=>"UA","field"=>"ua", "headerFilter"=>"input", "formatter"=>"textarea"], 
+            ["title"=>"Subs","field"=>"subs"], 
+            ["title"=>"Preland","field"=>"preland", "headerFilter"=>"input"], 
+            ["title"=>"Land","field"=>"land", "headerFilter"=>"input"]
+        ];
+        break;
+    }
+    return json_encode($columns);
+}
+
+function fill_table(string $table, array $header, array $data): string
+{
+    $countLines = 0;
+    foreach ($data as $item) {
+        $countLines++;
+        $table .= "<TR><TD>" . $countLines . "</TD>";
+        foreach ($header as $h) {
+            $h = strtolower($h);
+            $itemPart = $item[$h];
+            switch ($h) {
+                case 'subid':
+                    $itemPart = "<TD><a href='index.php?filter=single&subid={$itemPart}'>{$itemPart}</a></TD>";
+                    break;
+                case 'time':
+                    $itemPart = "<TD>" . date('Y-m-d H:i:s', $itemPart) . "</TD>";
+                    break;
+                case 'subs':
+                    $itemPart = "<TD>" . http_build_query($itemPart) . "</TD>";
+                    break;
+                case 'email':
+                    $itemPart = "<TD>" . (empty($itemPart) ? 'no' : $itemPart) . "</TD>";
+                    break;
+                default:
+                    if (is_array($itemPart)) {
+                        $itemPart = implode(',', $itemPart);
+                    }
+                    $itemPart = "<TD>{$itemPart}</TD>";
+                    break;
+            }
+            $table .= $itemPart;
+        }
+        $table .= "</TR>";
+    }
+    return $table;
+}
