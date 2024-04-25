247cd9f058073ad97b6a8249e1783a3d977871fc
Daniel Vygolov <dvygolov@users.noreply.github.com>
2024-04-25 22:15:06 +0400

    Update version.php
    correct version

--- admin/version.php
+++ admin/version.php
@@ -2,7 +2,7 @@
     <a href="https://yellowweb.top/donate" target="_blank">
         Version: 09.12.2023
         <br />
-        PHP: <?= phpversion() ?>
+        PHP: <?= PHP_VERSION ?>
         <br /> Please, donate!
     </a>
 </h2>
